9785206ebe4eb66d282d2d1e1532a67ffc9cdeec
Mako <mnfeconicu41@gmail.com>
2016-09-19 17:50:06 +0900

    create Direction map

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
 'use strict';
 
-var tokyoLatlng = { lat: 35.681298, lng: 139.766247 };
+var tokyo    = { lat: 35.681298, lng: 139.766247 };
+var yokohama = { lat: 35.466188, lng: 139.622715 };
 
 function initMap() {
   initBaseMap();
   initSatelliteMap();
+  initDirectionMap();
   initMarkerMap();
 }
 
 function initBaseMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('js_base_map'), {
-    center: tokyoLatlng,
+    center: tokyo,
     scrollwheel: false,
     zoom: 8
   });
@@ -20,7 +22,7 @@ function initBaseMap() {
 function initSatelliteMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('js_satellite_map'), {
-    center: tokyoLatlng,
+    center: tokyo,
     // Set mapTypeId to SATELLITE in order
     // to activate satellite imagery.
     mapTypeId: 'satellite',
@@ -29,10 +31,38 @@ function initSatelliteMap() {
   });
 }
 
+function initDirectionMap() {
+  var map = new google.maps.Map(document.getElementById('js_direction_map'), {
+    center: tokyo,
+    scrollwheel: false,
+    zoom: 7
+  });
+
+  var directionsDisplay = new google.maps.DirectionsRenderer({
+    map: map
+  });
+
+  // Set destination, origin and travel mode.
+  var request = {
+    destination: yokohama,
+    origin: tokyo,
+    travelMode: 'DRIVING'
+  };
+
+  // Pass the directions request to the directions service.
+  var directionsService = new google.maps.DirectionsService();
+  directionsService.route(request, function(response, status) {
+    if (status == 'OK') {
+      // Display the route on the map.
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
 function initMarkerMap() {
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('js_marker_map'), {
-    center: tokyoLatlng,
+    center: tokyo,
     scrollwheel: false,
     zoom: 4
   });
@@ -40,7 +70,7 @@ function initMarkerMap() {
   // Create a marker and set its position.
   var marker = new google.maps.Marker({
     map: map,
-    position: tokyoLatlng,
+    position: tokyo,
     title: 'Hello World!'
   });
 }
